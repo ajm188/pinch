3b7cdcbc91ff896b089776185c74f292671c124a
peterjeliot <peterjeliot@gmail.com>
2015-06-06 16:18:54 -0700

    Remove redundant user route/action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,4 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     render :show
   end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      render :show
-    else
-      render json: @user.errors.full_messages
-    end
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   root 'site#home'
   devise_for :users, controllers: { registrations: 'registrations' }
-
-  resources :users
 end
