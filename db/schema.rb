fae0326dc8a17bd1dac0732caa28f1f1d49e6c88
Andrew Mason <mason@case.edu>
2015-06-06 12:42:09 -0700

    add events table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150606193716) do
+ActiveRecord::Schema.define(version: 20150606194102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.integer  "non_profit_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "non_profits", force: :cascade do |t|
     t.string   "name"
