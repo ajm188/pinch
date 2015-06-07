8d1301c66c60f04d232196c247184f381c33e0a4
peterjeliot <peterjeliot@gmail.com>
2015-06-06 20:15:35 -0700

    Fix deep munge nilling empty arrays in JSON

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,10 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(process_params)
+      @user.professions = [] if process_params["professions"].nil?
+      @user.skills = [] if process_params["skills"].nil?
+      @user.interests = [] if process_params["interests"].nil?
+      @user.save
       render :show
     else
       render json: @user.errors.full_messages, status: :unprocessable_entity
@@ -21,17 +25,19 @@ class UsersController < ApplicationController
   end
 
   def process_params
-    processed_params = user_params
-    (processed_params["professions"] || []).map! do |hash|
+    return @processed_params if @processed_params
+
+    @processed_params = user_params
+    (@processed_params["professions"] || []).map! do |hash|
       Profession.where(name: hash["name"]).first_or_create
     end
-    (processed_params["interests"] || []).map! do |hash|
+    (@processed_params["interests"] || []).map! do |hash|
       Interest.where(name: hash["name"]).first_or_create
     end
-    (processed_params["skills"] || []).map! do |hash|
+    (@processed_params["skills"] || []).map! do |hash|
       Skill.where(name: hash["name"]).first_or_create
     end
 
-    processed_params
+    @processed_params
   end
 end
